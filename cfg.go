e4d1ca2ff4b42d505e85fee6a4caddea01a2f20e
Bart de Boer <bart.deboer@gmail.com>
2020-05-15 17:04:42 +0200

    Auto config and flags

--- cfg.go
+++ cfg.go
@@ -1,43 +1,129 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cfg
 
 import (
 	"fmt"
 	"os"
+	"reflect"
 
+	"github.com/iancoleman/strcase"
+	"github.com/imdario/mergo"
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
 var isInitialized bool
 
-func init() {}
-
 func Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error {
-	fmt.Print("Unmarshal\n")
 	initConfig()
 	return viper.Unmarshal(rawVal, opts...)
 }
 
 func UnmarshalKey(key string, rawVal interface{}, opts ...viper.DecoderConfigOption) error {
-	fmt.Print("UnmarshalKey\n")
 	initConfig()
-	return viper.UnmarshalKey(key, rawVal, opts...)
+	rvp := reflect.ValueOf(rawVal)
+	if k := rvp.Kind(); k != reflect.Ptr {
+		panic("Value is not a pointer")
+	}
+	rv := rvp.Elem() // struct value from pointer
+	if k := rv.Kind(); k != reflect.Struct {
+		panic("Value is not a struct")
+	}
+	curVal := rv.Interface() // Get real value of Value
+	if err := viper.UnmarshalKey(key, rawVal, opts...); err != nil {
+		return err
+	}
+	if err := mergo.MergeWithOverwrite(rawVal, curVal); err != nil {
+		return err
+	}
+	return nil
+}
+
+func BindPersistentFlags(c *cobra.Command, key string, rawVal interface{}) {
+	BindCommandFlags(c, c.PersistentFlags(), key, rawVal)
+}
+
+func BindFlags(c *cobra.Command, key string, rawVal interface{}) {
+	BindCommandFlags(c, c.Flags(), key, rawVal)
+}
+
+func BindCommandFlags(c *cobra.Command, flags *pflag.FlagSet, key string, rawVal interface{}) {
+	createFlags(flags, rawVal)
+	c.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		UnmarshalKey(key, rawVal)
+		updateFlags(flags, rawVal)
+	}
+	helpFunc := c.HelpFunc()
+	c.SetHelpFunc(func(cmd *cobra.Command, s []string) {
+		UnmarshalKey(key, rawVal)
+		updateFlags(flags, rawVal)
+		helpFunc(cmd, s)
+	})
+}
+
+func updateFlags(flags *pflag.FlagSet, rawVal interface{}) {
+	rvp := reflect.ValueOf(rawVal) // pointer struct value
+	rtp := reflect.TypeOf(rawVal)  // pointer struct type
+	if k := rvp.Kind(); k != reflect.Ptr {
+		panic("Value is not a pointer")
+	}
+	rv := rvp.Elem() // struct value from pointer
+	rt := rtp.Elem() // struct type from pointer
+	if k := rv.Kind(); k != reflect.Struct {
+		panic("Value is not a struct")
+	}
+	for i := 0; i < rv.NumField(); i++ {
+		fv := rv.Field(i) // value
+		ft := rt.Field(i) // struct field type
+		flag := flags.Lookup(strcase.ToKebab(ft.Name))
+		if flag != nil {
+			flag.DefValue = fmt.Sprintf("%v", fv.Interface())
+		}
+	}
+}
+
+func createFlags(flags *pflag.FlagSet, rawVal interface{}) {
+	// https://blog.golang.org/laws-of-reflection
+	rvp := reflect.ValueOf(rawVal) // pointer struct value
+	rtp := reflect.TypeOf(rawVal)  // pointer struct type
+	if k := rvp.Kind(); k != reflect.Ptr {
+		panic("Value is not a pointer")
+	}
+	rv := rvp.Elem() // struct value from pointer
+	rt := rtp.Elem() // struct type from pointer
+	if k := rv.Kind(); k != reflect.Struct {
+		panic("Value is not a struct")
+	}
+	for i := 0; i < rv.NumField(); i++ {
+		fv := rv.Field(i) // value
+		ft := rt.Field(i) // struct field type
+		flagName := strcase.ToKebab(ft.Name)
+		switch fv.Kind() {
+		case reflect.Bool:
+			flags.BoolVarP(
+				fv.Addr().Interface().(*bool),
+				flagName, "",
+				fv.Interface().(bool),
+				ft.Tag.Get("usage"))
+			break
+		case reflect.String:
+			flags.StringVarP(
+				fv.Addr().Interface().(*string),
+				flagName, "",
+				fv.Interface().(string),
+				ft.Tag.Get("usage"))
+			break
+		case reflect.Int:
+			flags.IntVarP(
+				fv.Addr().Interface().(*int),
+				flagName, "",
+				fv.Interface().(int),
+				ft.Tag.Get("usage"))
+			break
+		}
+	}
 }
 
 func Get(key string) interface{} {
@@ -58,7 +144,6 @@ func ReadInConfig() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	fmt.Print("Read config\n")
 	if isInitialized {
 		return
 	}
